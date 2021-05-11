aeef76bacc58db539199fcb24ebcc201f43fb4cf
patriciolavin <plavinp@gmail.com>
2021-05-11 18:50:34 -0400

    Viajes Chile v2.0

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $(function () {
+        $('[data-toggle="popover"]').popover();
+    });
+
+
+});
