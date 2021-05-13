1f16654a10070c2e580119a226f55a04774bdcd6
patriciolavin <plavinp@gmail.com>
2021-05-13 13:40:51 -0400

    Viajes Chile v3.2 - final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+
+
+    var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 100
+    }, 500);
+
+    return false;
+});
+
+
+
+
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
